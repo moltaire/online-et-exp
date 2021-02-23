a9cb6a18df17ef430067e52aebf7a53c115b3ef9
Felix Molter (MBPr) <moltaire1@gmail.com>
2021-02-23 08:04:37 +0100

    pm demo

--- js/custom_jspsych-plugins/two-gamble-sequence.js
+++ js/custom_jspsych-plugins/two-gamble-sequence.js
@@ -128,9 +128,8 @@ jsPsych.plugins["two-gamble-sequence"] = (function () {
 
     //--------Set up Canvas start-------
     var gambleCanvas = document.createElement("canvas");
-    gambleCanvas.width = screen.width;
-    gambleCanvas.height = screen.height;
-    console.log(screen.width, "x", screen.height);
+    gambleCanvas.width = 0.95 * screen.width;
+    gambleCanvas.height = 0.95 * screen.height;
     display_element.appendChild(gambleCanvas);
 
     function drawPiechart(
